8e8e8540ae5a8d20efbcdf4712ae856fca3f381e
Richard Enrico <54946587+richardenrico@users.noreply.github.com>
2022-08-29 11:39:21 +0800

    add input user feature

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    
     news = Article.objects(category='berita').order_by('-posted_at')[:4]
     announcement = Article.objects(category='pengumuman').order_by('-posted_at').first()
     return render_template('layout.html', news=news, announcement=announcement)
@@ -40,60 +41,113 @@ def detail(endpoint, slug):
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    
-    return render_template('dashboard.html')
+    if 'username' in session :
+        user = User.objects(username = session.get('username')).first()
+        return render_template('dashboard.html', role=user.role)
+    return redirect(url_for('login'))
 
 @app.route('/dashboard/<endpoint>', methods=['GET', 'POST'])
 def view(endpoint):
-    article = Article.objects(category=endpoint).order_by('-posted_at')
-    return render_template('dashboard.html', data=article, endpoint=endpoint)
+    if 'username' in session:
+        user_role = User.objects(username = session.get('username')).first().role
+        if user_role == 'Admin':
+            user = User.objects().order_by('name')
+            return render_template('dashboard.html', user=user, role=user_role, endpoint=endpoint)
+        else:    
+            article = Article.objects(category=endpoint).order_by('-posted_at')
+            return render_template('dashboard.html', data=article, endpoint=endpoint)
+    return redirect(url_for('login'))
 
 @app.route('/input/<endpoint>', methods=['GET', 'POST'])
 def input(endpoint):
     if 'username' in session:
-        article = Article.objects()
-        return render_template('input_forms.html', data=article, endpoint=endpoint)
+        isAdmin = User.objects(username=session.get('username')).first()
+        if endpoint == 'user':
+            if isAdmin.role == 'Admin':
+                user = User.objects()
+                return render_template('input_forms.html', user=user, role=isAdmin.role, endpoint=endpoint)
+            else:
+                return 'No Access'
+        else:
+            article = Article.objects()
+            return render_template('input_forms.html', data=article, endpoint=endpoint)
     return redirect(url_for('login'))
 
 @app.route('/save/<endpoint>', methods=['GET', 'POST'])
 def save(endpoint):
     if 'username' in session:
+        user_role = User.objects(username = session.get('username')).first()
         object_id = request.form['id']
         
         if object_id:
-            article = Article.objects(id=object_id).first()
-            article.update(
-                title = request.form['title'],
-                cover = request.form['cover'],
-                content = request.form['content'],
-                slug = '-'.join(request.form['title'].strip().lower().split(' ')),
-                updated_at = datetime.datetime.utcnow()
-            )
+            if endpoint == 'user':
+                if user_role.role == 'Admin':
+                    user = User.objects(id=object_id).first()
+                    user.update(
+                        username = request.form['username'],
+                        name = request.form['name'],
+                        password = request.form['password']
+                    )
+                else:
+                    return 'No Access'
+            else:
+                article = Article.objects(id=object_id).first()
+                article.update(
+                    title = request.form['title'],
+                    cover = request.form['cover'],
+                    content = request.form['content'],
+                    slug = '-'.join(request.form['title'].strip().lower().split(' ')),
+                    updated_at = datetime.datetime.utcnow()
+                )
         else:
-            author = User.objects(username = session.get('username')).first()
-            
-            article = Article(title=request.form['title'])
-            article.author = author.name
-            article.cover = request.form['cover']
-            article.content = request.form['content']
-            article.slug = '-'.join(request.form['title'].strip().lower().split(' '))
-            article.category = endpoint
-            
-            article.save()
+            if endpoint == 'user':
+                if user_role.role == 'Admin':
+                    user = User(username=request.form['username'])
+                    user.name = request.form['name']
+                    user.password = request.form['password']
+                    user.role = request.form['role']
+                    
+                    user.save()
+            else:
+                author = User.objects(username = session.get('username')).first()
+                
+                article = Article(title=request.form['title'])
+                article.author = author.name
+                article.cover = request.form['cover']
+                article.content = request.form['content']
+                article.slug = '-'.join(request.form['title'].strip().lower().split(' '))
+                article.category = endpoint
+                
+                article.save()
         return redirect(url_for('dashboard'))
     return redirect(url_for('login'))
 
-@app.route('/edit/<endpoint>/<article_id>', methods=['GET', 'POST'])
-def edit(article_id, endpoint):
+@app.route('/edit/<endpoint>/<id>', methods=['GET', 'POST'])
+def edit(id, endpoint):
+    role = User.objects(username=session.get('username')).first().role
     if 'username' in session:
-        article = Article.objects(id=article_id).first()
-        return render_template('input_forms.html', data=article, endpoint=endpoint)
+        if endpoint == 'user':
+            if role == 'Admin':
+                user = User.objects(id=id).first()
+                return render_template('input_forms.html', user=user, endpoint=endpoint)
+        else:
+            article = Article.objects(id=id).first()
+            return render_template('input_forms.html', data=article, endpoint=endpoint)
     return redirect(url_for('login'))
 
-@app.route('/delete', methods=['GET', 'POST'])
-def delete():
-    Article.objects(id=request.form['id']).delete()
-    return redirect(url_for('dashboard'))
+@app.route('/delete/<endpoint>', methods=['GET', 'POST'])
+def delete(endpoint):
+    role = User.objects(username=session.get('username')).first().role
+    if 'username' in session:
+        if endpoint == 'user':
+            if role == 'Admin':
+                User.objects(id=request.form['id']).delete()
+            else:
+                return 'No Access'
+        else:
+            Article.objects(id=request.form['id']).delete()
+        return redirect(url_for('dashboard'))
+    return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
